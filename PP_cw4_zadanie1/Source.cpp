5671e36f6fd21566a09e384a92de6b938ff06e06
Miroslaw Puczynski <miroslaw@puczynski.org>
2019-11-22 06:14:56 +0100

    002

--- PP_cw4_zadanie1/Source.cpp
+++ PP_cw4_zadanie1/Source.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 long long oblicz_silnie(int n) {
 	long long s = 1;
+	if (n < 0) return -1;
+	if (n > 20) return -2;
 	for (int i = 2; i <= n; i++) {
 		s = s * i;
 	}
@@ -19,7 +21,15 @@ int main() {
 	cin >> a >> b;
 	for (int i = a; i <= b; i++) {
 		s = oblicz_silnie(i);
-		cout << "\n" << ((i < 10) ? " " : "") << i << "! = " << s;
+		if (s == -1) {
+			cout << "\n" << "ujemna wartosc n";
+		}
+		else if (s == -2) {
+			cout << "\n" << "zbyt duza wartosc n";
+		}
+		else {
+			cout << "\n" << ((i < 10) ? " " : "") << i << "! = " << s;
+		}
 	}
 
 }
